62e1b87a9626c8af09f124d384e9f25ec3fd2e62
Rusty12321 <weston@hilgenfeld.org>
2023-03-24 14:42:24 -0500

    express

--- httpServer.js
+++ httpServer.js
@@ -1,43 +1,31 @@
+const { json } = require('express');
+const express = require('express');
 const fs = require('fs');
-const http = require('http');
-const petRegExp = /^\/pets\/(.*)$/;
-const port = 8000;
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log('incoming response');
-    fs.readFile('pets.json', 'utf8', (error, data) => {
-        pets = JSON.parse(data);
-        // console.log(petRegExp.test(req.url))
-        // console.log(req.url)
-        let index = req.url.split('/')
-        if (error) {
-            console.error(error)
-        } else if (req.method === 'GET' && index[1] === 'pets' && index[2] === undefined || (index[2] >= 0 && index[2] <pets.length)) {
-            for (let i = 0; i < pets.length; i++) {
-                if (req.url === '/pets') {
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.write(data);
-                    res.end();
-                    break;
-                } else if (req.url === `/pets/${i}`) {
-                    let currentPet = JSON.stringify(pets[i]);
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.write(currentPet);
-                    res.end();
-                    break;
-                } 
-            }
+
+fs.readFile('pets.json', 'utf8', (error, data) => {
+    pets = JSON.parse(data);
+app.get('/pets', (req, res) => {
+        res.type('application/json');
+        res.send(pets)
+    })
+    
+    app.get('/pets/:petIndex', (req, res, next) => {
+        let index = req.params.petIndex
+        if (index === undefined || (index >= 0 && index < pets.length)) {
+            res.status(200);
+            res.type('application/json');
+            res.send(pets[index]);
         } else {
-            res.writeHead(404, {'Content-Type': 'text/plain'})
-            res.end('Not Found');
+            res.status(404);
+            res.type('text/plain');
+            res.send('Not Found');
         }
     })
 })
 
-server.listen(port, (error)  => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(`Server is running on ${port}`);
-    }
+app.listen(port, () => {
+    console.log(`server is listening on port:${port}`);
 })
