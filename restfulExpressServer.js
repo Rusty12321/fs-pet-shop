9ae3aade1fd4b4702679caf18964fc79f7387fca
Rusty12321 <weston@hilgenfeld.org>
2023-04-05 17:51:57 -0500

    complete CRUD commands for db

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,100 +1,89 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const { log } = require('console');
-const port = 3000;
+// const bodyParser = require("body-parser");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const pg = require("pg");
+const { Pool } = pg;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const port = process.env.PORT || 3000;
+pool.connect();
 
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-fs.readFile("pets.json", "utf8", (error, data) => {
-    pets = JSON.parse(data);
-    
-    app.post("/pets", (req, res) => {
-        let { age, kind, name } = req.body;
-        console.log(age, kind, name)
-        if (age === undefined || kind === undefined || name === undefined || isNaN(age)) {
-          res.status(400);
-          res.type("text/plain");
-          res.send("Bad Request");
-        } else {
-          age = Number.parseInt(age);
-          let newPet = { age: age, kind: kind, name: name };
-          res.status(200);
-          res.type("application/json");
-          res.send(newPet);
-          pets.push(newPet);
-          fs.writeFile("pets.json", `${JSON.stringify(pets)}`, () => {});
-        }
-      });
+app.get("/pets", (req, res) => {
+  pool.query("SELECT * FROM pet").then((result) => {
+    res.send(result.rows);
+  });
+});
 
-    app.patch("/pets/:petId", (req, res) => {
-        let id = req.params.petId;
-        let { age, kind, name } = req.body;
-        if (id === undefined || (id >= 0 && id < pets.length)) {
-            if ((kind === undefined && name === undefined && age === undefined) || (age !== undefined && isNaN(age))) {
-                res.status(400);
-                res.type("text/plain");
-                res.send("Bad Request");
-            } else {
-                for (let char in pets[id]) {
-                    let body = req.body[char];
-                    if (body !== undefined) {
-                        pets[id][char] = body;
-                    }
-                }
-                res.status(200);
-                res.type("application/json");
-                res.send(pets[id]);
-                fs.writeFile("pets.json", `${JSON.stringify(pets)}`, () => {});
-            }
-        } else {
-            res.type("text/plain");
-            res.status(404);
-            res.send("Not Found");
-        }
-    })
-
-    app.delete("/pets/:petId", (req, res) => {
-        let id = req.params.petId;
-        if (id === undefined || (id >= 0 && id < pets.length)) {
-            res.status(200);
-            res.type("application/json");
-            res.send(pets[id])
-            pets.splice(id, 1);
-            fs.writeFile("pets.json", `${JSON.stringify(pets)}`, () => {});
-        } else {
-            res.type("text/plain");
-            res.status(404);
-            res.send("Not Found");
-        }
-    })
-  
-    app.get("/pets", (req, res) => {
-      res.type("application/json");
-      res.send(pets);
-    });
-  
-    app.get("/pets/:petId", (req, res) => {
-      let id = req.params.petId;
-      if (id === undefined || (id >= 0 && id < pets.length)) {
-        res.type("application/json");
-        res.status(200);
-        res.send(pets[id]);
+app.get("/pets/:petID", (req, res) => {
+  pool
+    .query(`SELECT * FROM pet WHERE id = $1;`, [req.params.petID])
+    .then((result) => {
+      if (result.rows.length == 0) {
+        res.status(404).send("Not Found");
       } else {
-        res.type("text/plain");
-        res.status(404);
-        res.send("Not Found");
+        res.send(result.rows);
       }
     });
-  
-    app.get("/*", (req, res) => {
-      res.type("text/plain");
-      res.sendStatus(404, "Not Found");
+});
+
+app.post("/pets", (req, res) => {
+  if (isNaN(req.body.age)) {
+    res.status(400).send("Bad Request");
+  } else {
+    pool
+      .query(
+        `INSERT INTO pet (age, kind, name) VALUES ($1, $2, $3) RETURNING *`,
+        [req.body.age, req.body.kind, req.body.name]
+      )
+      .then((result) => {
+        res.status(201).send(result.rows);
+      });
+  }
+});
+
+app.patch("/pets/:petID", (req, res) => {
+  if (isNaN(req.body.age) && req.body.age !== undefined) {
+    res.status(400).send("Bad Request");
+  } else {
+    let key = Object.keys(req.body)[0];
+    let value = Object.values(req.body)[0];
+    pool
+      .query(`UPDATE pet SET ${key} = $1 WHERE id = $2 RETURNING *`, [
+        value,
+        req.params.petID,
+      ])
+      .then((result) => {
+        if (result.rows.length == 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.status(202).send(result.rows);
+        }
+      });
+  }
+});
+
+app.delete("/pets/:petID", (req, res) => {
+  pool
+    .query(`DELETE FROM pet WHERE id = $1 RETURNING *`, [req.params.petID])
+    .then((result) => {
+      if (result.rows.length == 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.status(202).send(result.rows);
+      }
     });
-  });
-  
-  app.listen(port, () => {
-    console.log(`server is listening on port:${port}`);
-  });
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).send("Not Found");
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`);
+});
